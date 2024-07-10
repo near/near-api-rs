ec77a98efeb6726814e2f86f7c8d146eb2c4468f
akorchyn <artur.yurii.korchynskyi@gmail.com>
2024-07-10 17:58:32 +0300

    feat: account add_key, delete_key, delete_account

--- src/account.rs
+++ src/account.rs
@@ -1,9 +1,16 @@
 use near_crypto::PublicKey;
-use near_primitives::types::AccountId;
+use near_primitives::{
+    account::{AccessKey, AccessKeyPermission},
+    action::{AddKeyAction, DeleteKeyAction},
+    types::AccountId,
+};
 use near_token::NearToken;
 
-use crate::query::{
-    AccessKeyHandler, AccessKeyListHandler, AccountViewHandler, CallResultHandler, QueryBuilder,
+use crate::{
+    query::{
+        AccessKeyHandler, AccessKeyListHandler, AccountViewHandler, CallResultHandler, QueryBuilder,
+    },
+    transactions::ConstructTransaction,
 };
 
 pub struct Account(pub AccountId);
@@ -54,6 +61,74 @@ impl Account {
             request,
             CallResultHandler::with_postprocess(NearToken::from_yoctonear),
         ))
+    }
+
+    pub fn add_key(&self, access_key: AccessKeyPermission) -> AddKeyBuilder {
+        AddKeyBuilder {
+            account_id: self.0.clone(),
+            access_key,
+        }
+    }
+
+    pub fn delete_key(&self, public_key: PublicKey) -> ConstructTransaction {
+        ConstructTransaction::new(self.0.clone(), self.0.clone()).add_action(
+            near_primitives::transaction::Action::DeleteKey(Box::new(DeleteKeyAction {
+                public_key,
+            })),
+        )
+    }
+
+    pub fn delete_account(&self, beneficiary_id: AccountId) -> ConstructTransaction {
+        ConstructTransaction::new(self.0.clone(), self.0.clone()).add_action(
+            near_primitives::transaction::Action::DeleteAccount(
+                near_primitives::transaction::DeleteAccountAction { beneficiary_id },
+            ),
+        )
+    }
+
+    // pub async fn delegations(&self) -> anyhow::Result<BTreeMap<AccountId, NearToken>> {
+    //     let validators = if let Ok(fastnear) = self.client.fastnear() {
+    //         fastnear.account_delegated_in(&self.account_id).await?
+    //     } else if let Ok(staking) = self.client.stake() {
+    //         staking.staking_pools().await?
+    //     } else {
+    //         bail!("FastNear and Staking pool factory are not set");
+    //     };
+
+    //     futures::stream::iter(validators)
+    //         .map(|validator_account_id| async {
+    //             let balance = self.delegation_in_pool(&validator_account_id).await?;
+    //             Ok::<_, anyhow::Error>((validator_account_id, balance))
+    //         })
+    //         .buffer_unordered(self.client.concurrency_limit)
+    //         .filter(|balance_result| {
+    //             futures::future::ready(if let Ok((_, balance)) = balance_result {
+    //                 !balance.is_zero()
+    //             } else {
+    //                 true
+    //             })
+    //         })
+    //         .try_collect()
+    //         .await
+    // }
+}
+
+pub struct AddKeyBuilder {
+    account_id: AccountId,
+    access_key: AccessKeyPermission,
+}
+
+impl AddKeyBuilder {
+    pub fn with_public_key(self, public_key: PublicKey) -> ConstructTransaction {
+        ConstructTransaction::new(self.account_id.clone(), self.account_id).add_action(
+            near_primitives::transaction::Action::AddKey(Box::new(AddKeyAction {
+                access_key: AccessKey {
+                    nonce: 0,
+                    permission: self.access_key,
+                },
+                public_key,
+            })),
+        )
     }
 }
 
