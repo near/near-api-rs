0059799f2f4e1237f8e20e2201a9665f10a5a976
akorchyn <artur.yurii.korchynskyi@gmail.com>
2024-09-12 14:46:42 +0300

    bugfix: compilation

--- src/types/storage.rs
+++ src/types/storage.rs
@@ -1,3 +1,5 @@
+use serde::de::{Deserialize, Deserializer};
+
 // Taken from https://github.com/bos-cli-rs/near-socialdb-client-rs/blob/main/src/lib.rs
 #[derive(Debug, Clone, serde::Deserialize)]
 pub struct StorageBalance {
@@ -5,4 +7,13 @@ pub struct StorageBalance {
     pub available: u128,
     #[serde(deserialize_with = "parse_u128_string")]
     pub total: u128,
+}
+
+fn parse_u128_string<'de, D>(deserializer: D) -> Result<u128, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    <std::string::String as Deserialize>::deserialize(deserializer)?
+        .parse::<u128>()
+        .map_err(serde::de::Error::custom)
 }
