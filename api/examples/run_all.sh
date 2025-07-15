01f289b17d080cb6493ad9271917581cb360d01a
akorchyn <artur.yurii.korchynskyi@gmail.com>
2025-07-15 15:09:20 +0300

    run_all.sh

---
+++ api/examples/run_all.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+# Run all examples
+for example in $(find . -name "*.rs" -type f); do
+    example_name=$(basename $example .rs)
+
+    echo "--------------------------------"
+    echo "Running $example_name"
+    echo "--------------------------------"
+    CI=true cargo run --release --example $example_name
+    echo "--------------------------------"
+done
