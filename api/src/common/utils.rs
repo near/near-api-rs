ba06382c52c09ae3a402af92716e156c53ca4f55
akorchyn <artur.yurii.korchynskyi@gmail.com>
2025-07-15 18:48:44 +0300

    feat: removed is_critical_error

--- api/src/common/utils.rs
+++ api/src/common/utils.rs
@@ -17,94 +17,35 @@ pub const fn near_data_to_near_token(data: near_types::Data<u128>) -> NearToken 
     NearToken::from_yoctonear(data.data)
 }
 
-pub fn is_critical_blocks_error(err: &RpcError) -> bool {
-    // is_critical_json_rpc_error(err, |err| match err {
-    //     near_openapi_client::methods::block::RpcBlockError::UnknownBlock { .. }
-    //     | near_openapi_client::methods::block::RpcBlockError::NotSyncedYet
-    //     | near_openapi_client::methods::block::RpcBlockError::InternalError { .. } => true,
-    // })
+// TODO: this is a temporary solution to check if an error is critical
+// we had previously a full scale support for that
+// but auto generated code doesn't support errors yet, so we would need to leave it as is for now
+// We default to false as we can't know if an error is critical or not without the types
+// so to keep it safe it's better to retry
 
-    // TODO: implement this
-    false
+pub fn is_critical_blocks_error(err: &RpcError) -> bool {
+    is_critical_json_rpc_error(err, |_| false)
 }
 
 pub fn is_critical_validator_error(err: &RpcError) -> bool {
-    // is_critical_json_rpc_error(err, |err| match err {
-    //     near_jsonrpc_primitives::types::validator::RpcValidatorError::UnknownEpoch
-    //     | near_jsonrpc_primitives::types::validator::RpcValidatorError::ValidatorInfoUnavailable
-    //     | near_jsonrpc_primitives::types::validator::RpcValidatorError::InternalError { .. } => {
-    //         true
-    //     }
-    // })
-    // TODO: implement this
-    false
+    is_critical_json_rpc_error(err, |_| false)
 }
 
 pub fn is_critical_query_error(rpc_error: &RpcError) -> bool {
-    // is_critical_json_rpc_error(err, |err| match err {
-    //     near_jsonrpc_primitives::types::query::RpcQueryError::NoSyncedBlocks
-    //     | near_jsonrpc_primitives::types::query::RpcQueryError::UnavailableShard { .. }
-    //     | near_jsonrpc_primitives::types::query::RpcQueryError::GarbageCollectedBlock { .. }
-    //     | near_jsonrpc_primitives::types::query::RpcQueryError::UnknownBlock { .. }
-    //     | near_jsonrpc_primitives::types::query::RpcQueryError::InvalidAccount { .. }
-    //     | near_jsonrpc_primitives::types::query::RpcQueryError::UnknownAccount { .. }
-    //     | near_jsonrpc_primitives::types::query::RpcQueryError::NoContractCode { .. }
-    //     | near_jsonrpc_primitives::types::query::RpcQueryError::TooLargeContractState { .. }
-    //     | near_jsonrpc_primitives::types::query::RpcQueryError::UnknownAccessKey { .. }
-    //     | near_jsonrpc_primitives::types::query::RpcQueryError::ContractExecutionError { .. }
-    //     | near_jsonrpc_primitives::types::query::RpcQueryError::InternalError { .. } => true,
-    // })
-    // TODO: implement this
-    false
+    is_critical_json_rpc_error(rpc_error, |_| false)
 }
 
 pub fn is_critical_transaction_error(err: &RpcError) -> bool {
-    // is_critical_json_rpc_error(err, |err| {
-    //     match err {
-    //         near_openapi_client::methods::broadcast_tx_commit::RpcTransactionError::TimeoutError => {
-    //             false
-    //         }
-    //         near_openapi_client::methods::broadcast_tx_commit::RpcTransactionError::InvalidTransaction { .. } |
-    //             near_openapi_client::methods::broadcast_tx_commit::RpcTransactionError::DoesNotTrackShard |
-    //                 near_openapi_client::methods::broadcast_tx_commit::RpcTransactionError::RequestRouted{..} |
-    //                     near_openapi_client::methods::broadcast_tx_commit::RpcTransactionError::UnknownTransaction{..} |
-    //                         near_openapi_client::methods::broadcast_tx_commit::RpcTransactionError::InternalError{..} => {
-    //             true
-    //         }
-    //     }
-    // })
-    // TODO: implement this
-    false
+    is_critical_json_rpc_error(err, |_| false)
 }
 
-fn is_critical_json_rpc_error<T>(err: &RpcError, is_critical_t: impl Fn(&T) -> bool) -> bool {
-    // match err {
-    //     near_openapi_client::errors::JsonRpcError::TransportError(_rpc_transport_error) => {
-    //         false
-    //     }
-    //     near_openapi_client::errors::JsonRpcError::ServerError(rpc_server_error) => match rpc_server_error {
-    //         near_openapi_client::errors::JsonRpcServerError::HandlerError(rpc_transaction_error) => is_critical_t(rpc_transaction_error),
-    //         near_openapi_client::errors::JsonRpcServerError::RequestValidationError(..) |
-    //         near_openapi_client::errors::JsonRpcServerError::NonContextualError(..) => {
-    //             true
-    //         }
-    //         near_openapi_client::errors::JsonRpcServerError::InternalError{ .. } => {
-    //             false
-    //         }
-    //         near_openapi_client::errors::JsonRpcServerError::ResponseStatusError(json_rpc_server_response_status_error) => match json_rpc_server_response_status_error {
-    //             near_openapi_client::errors::JsonRpcServerResponseStatusError::Unauthorized |
-    //             near_openapi_client::errors::JsonRpcServerResponseStatusError::Unexpected{..} |
-    //             near_openapi_client::errors::JsonRpcServerResponseStatusError::BadRequest => {
-    //                 true
-    //             }
-    //             near_openapi_client::errors::JsonRpcServerResponseStatusError::TimeoutError |
-    //             near_openapi_client::errors::JsonRpcServerResponseStatusError::ServiceUnavailable |
-    //             near_openapi_client::errors::JsonRpcServerResponseStatusError::TooManyRequests => {
-    //                 false
-    //             }
-    //         }
-    //     }
-    // }
-    // TODO: implement this
-    false
+fn is_critical_json_rpc_error(
+    err: &RpcError,
+    is_critical_t: impl Fn(&serde_json::Value) -> bool,
+) -> bool {
+    match err {
+        RpcError::Variant0 { .. } => true,
+        RpcError::Variant1 { cause, .. } => is_critical_t(cause),
+        RpcError::Variant2 { .. } => false,
+    }
 }
