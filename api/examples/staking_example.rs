215fdbd3dd8cfa212d9da9a8abd674e2feb3960d
akorchyn <artur.yurii.korchynskyi@gmail.com>
2025-11-10 23:54:20 +0200

    fixed review after adding assert_failure

--- api/examples/staking_example.rs
+++ api/examples/staking_example.rs
@@ -1,74 +1,24 @@
-use near_api::{types::AccessKey, AccountId, NearToken, Signer, Staking};
-use near_sandbox::{config::DEFAULT_GENESIS_ACCOUNT_PRIVATE_KEY, GenesisAccount, SandboxConfig};
+use std::sync::Arc;
+
+use near_api::{AccountId, NearToken, NetworkConfig, RPCEndpoint, Signer, Staking};
+use near_sandbox::{
+    config::DEFAULT_GENESIS_ACCOUNT_PRIVATE_KEY, sandbox::patch::StateRecord, FetchData,
+};
 
 #[tokio::main]
 async fn main() {
     let staker: AccountId = "dev.near".parse().unwrap();
-    let sandbox = near_sandbox::Sandbox::start_sandbox_with_config(SandboxConfig {
-        additional_accounts: vec![GenesisAccount::default_with_name(staker.clone()).clone()],
-        ..SandboxConfig::default()
-    })
-    .await
-    .unwrap();
+    let sandbox = near_sandbox::Sandbox::start_sandbox().await.unwrap();
     let network =
         near_api::NetworkConfig::from_rpc_url("sandbox", sandbox.rpc_addr.parse().unwrap());
 
+    sandbox.create_account(staker.clone()).send().await.unwrap();
     let signer = Signer::new(Signer::from_secret_key(
         DEFAULT_GENESIS_ACCOUNT_PRIVATE_KEY.parse().unwrap(),
     ))
     .unwrap();
 
-    // Set-up staking pool.
-    let staking_pool: AccountId = "qbit.poolv1.near".parse().unwrap();
-    let mut pool_account = near_api::Account(staking_pool.clone())
-        .view()
-        .fetch_from_mainnet()
-        .await
-        .unwrap()
-        .data;
-    pool_account.locked = NearToken::from_near(0);
-    let pool_code = near_api::Contract(staking_pool.clone())
-        .wasm()
-        .fetch_from_mainnet()
-        .await
-        .unwrap()
-        .data
-        .code_base64;
-    sandbox
-        .patch_state(staking_pool.clone())
-        .account(pool_account)
-        .access_key(
-            signer.get_public_key().await.unwrap().to_string(),
-            AccessKey {
-                nonce: 0.into(),
-                permission: near_api::types::AccessKeyPermission::FullAccess,
-            },
-        )
-        .code(pool_code)
-        .send()
-        .await
-        .unwrap();
-
-    // Init staking pool
-    near_api::Contract(staking_pool.clone())
-        .call_function(
-            "new",
-            serde_json::json!({
-                "owner_id": staking_pool,
-                "stake_public_key": "KuTCtARNzxZQ3YvXDeLjx83FDqxv2SdQTSbiq876zR7",
-                "reward_fee_fraction": {
-                    "numerator": 0,
-                    "denominator": 100,
-                }
-            }),
-        )
-        .unwrap()
-        .transaction()
-        .with_signer(staking_pool.clone(), signer.clone())
-        .send_to(&network)
-        .await
-        .unwrap()
-        .assert_success();
+    let staking_pool = setup_staking_pool(&sandbox, &network, signer.clone()).await;
 
     let staker_delegation = Staking::delegation(staker.clone());
     staker_delegation
@@ -149,12 +99,58 @@ async fn main() {
     assert_eq!(balance.total, NearToken::from_near(4));
 
     // Can't be withdrawn yet as it should pass the minimum withdrawal period
-    assert!(staker_delegation
+    staker_delegation
         .withdraw_all(staking_pool.clone())
         .unwrap()
         .with_signer(signer.clone())
         .send_to(&network)
         .await
         .unwrap()
-        .is_failure());
+        .assert_failure();
+}
+
+async fn setup_staking_pool(
+    sandbox: &near_sandbox::Sandbox,
+    network: &NetworkConfig,
+    signer: Arc<Signer>,
+) -> AccountId {
+    let staking_pool: AccountId = "qbit.poolv1.near".parse().unwrap();
+    let mut patch = sandbox
+        .patch_state(staking_pool.clone())
+        .with_default_access_key()
+        .fetch_from(RPCEndpoint::mainnet().url, FetchData::NONE.account().code())
+        .await
+        .unwrap();
+
+    // Set locked as zero, so we can initialize the pool
+    patch.state.iter_mut().for_each(|e| {
+        if let StateRecord::Account { account, .. } = e {
+            account["locked"] = "0".into();
+        }
+    });
+
+    patch.send().await.unwrap();
+
+    // Init staking pool
+    near_api::Contract(staking_pool.clone())
+        .call_function(
+            "new",
+            serde_json::json!({
+                "owner_id": staking_pool,
+                "stake_public_key": "KuTCtARNzxZQ3YvXDeLjx83FDqxv2SdQTSbiq876zR7",
+                "reward_fee_fraction": {
+                    "numerator": 0,
+                    "denominator": 100,
+                }
+            }),
+        )
+        .unwrap()
+        .transaction()
+        .with_signer(staking_pool.clone(), signer)
+        .send_to(network)
+        .await
+        .unwrap()
+        .assert_success();
+
+    staking_pool
 }
