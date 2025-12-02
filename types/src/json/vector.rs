070a3b6a4227079580c5e240dbdb82db0116f6f5
Artur Yurii Korchynskyi <42449190+akorchyn@users.noreply.github.com>
2025-12-03 00:00:12 +0200

    feat: updated acount id to v2, updated openapi-types, fixed DeterministicStateInit action serialization (#97)
    * feat: updated acount id to v2, updated openapi-types, fixed DeterministicStateInit action serialization
    
    * fixed spellchecker
    
    * bumped up version of account id

--- types/src/json/vector.rs
+++ types/src/json/vector.rs
@@ -1,10 +1,11 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Deserialize, Serialize};
+use serde_with::base64::Base64;
+use serde_with::serde_as;
 
-use crate::utils::base64_bytes;
-
+#[serde_as]
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
-pub struct Base64VecU8(#[serde(with = "base64_bytes")] pub Vec<u8>);
+pub struct Base64VecU8(#[serde_as(as = "Base64")] pub Vec<u8>);
 
 impl From<Vec<u8>> for Base64VecU8 {
     fn from(v: Vec<u8>) -> Self {
--- types/src/utils/mod.rs
+++ types/src/utils/mod.rs
@@ -1,26 +1,3 @@
-/// Convenience module to allow annotating a serde structure as base64 bytes.
-pub mod base64_bytes {
-    use base64::Engine;
-    use serde::{de, Deserialize, Deserializer, Serializer};
-
-    pub fn serialize<S>(bytes: &[u8], serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_str(&base64::engine::general_purpose::STANDARD.encode(bytes))
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s: String = Deserialize::deserialize(deserializer)?;
-        base64::engine::general_purpose::STANDARD
-            .decode(s.as_str())
-            .map_err(de::Error::custom)
-    }
-}
-
 pub mod near_gas_as_u64 {
     use near_gas::NearGas;
     use serde::Serializer;
